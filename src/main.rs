b5b8b3608d4ba2fd5fc3e24c21f35f76e2f9a31e
soupersauce <soupersauce@gmail.com>
2019-05-20 19:08:33 -0700

    First steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate mio;
+use mio::*;
+use mio::tcp::*;
+use std::net::SocketAddr;
+
+struct WebSocketServer;
+
+impl Handler for WebSocketServer {
+    //Traits can have useful default implementations, so in fact the handler
+    //interface requres us to provide only two things: concrete types for
+    //timeouts and messages.
+    //We're not ready to cover these fancy details, and we wouldn't get to them
+    //anytime soon, so let's get along with the defauls from the mio exampes:
+    type Timeout = usize;
+    type Message = ();
+}
 fn main() {
-    println!("Hello, world!");
+    let address = "0.0.0.0:10000".parse::<SocketAddr>().unwrap(); 
+    let server_socket = TcpListener::bind(&address).unwrap();
+    let mut event_loop = EventLoop::new().unwrap();
+    // Create a new instance of our handler struct:
+    let mut handler = WebSocketServer;
+
+    event_loop.register(&server_socket,
+                        Token(0),
+                        EventSet::readable(),
+                        PollOpt::edge()).unwrap();
+    // ... and provide the even loop with a mutable reference to it:
+    event_loop.run(&mut handler).unwrap();
 }
